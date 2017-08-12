8f65ca7d73a3d8445cbc9947f399dc25cd153671
rajkumar888 <rajkumar888@gmail.com>
2017-08-12 20:38:17 +0530

    ff

---
+++ src/demoseleniumlearning/SeleniumFirefoxlanuch.java
@@ -0,0 +1,25 @@
+package demoseleniumlearning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+class SeleniumFirefoxlanuch {
+
+	public static void main(String[] args) 
+	{
+		//System.setProperty("webdriver.gecko.driver", "C:\\Driver\\geckodriver-v0.18.0-win32\\geckodriver.exe");
+		
+		// for giving the browser's driver path
+		
+				
+				
+			WebDriver driver= new FirefoxDriver();			// for launching the browser
+			
+			driver.get("http://www.facebook.com");			// for open a specific url
+			
+			System.out.println(driver.getTitle());			// this is to print the title on the console
+			
+			
+			}
+
+}
